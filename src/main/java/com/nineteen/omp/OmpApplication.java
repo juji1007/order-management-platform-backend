22e357c91aee23188689eb5b1de721913fbd88ae
Choi Hojin <jkhh9725@gmail.com>
2025-02-13 16:04:09 +0900

    first commit

---
+++ src/main/java/com/nineteen/omp/OmpApplication.java
@@ -0,0 +1,13 @@
+package com.nineteen.omp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nineteen/omp/OmpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nineteen.omp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OmpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
