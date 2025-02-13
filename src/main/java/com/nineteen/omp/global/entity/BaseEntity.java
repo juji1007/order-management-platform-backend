794b9ae8a6577c61e6fd5009a7e4035baabfc17c
jeonjuho <wjswngh23@daum.net>
2025-02-13 23:07:15 +0900

    test(BaseEntityTest): BaseEntity 클래스의 테스트 코드 추가
    - 타임 스탬프 역할을 적절히 수행하는지 검증하는 테스트 코드 추가

---
+++ src/main/java/com/nineteen/omp/global/entity/BaseEntity.java
@@ -0,0 +1,47 @@
+package com.nineteen.omp.global.entity;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PreRemove;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+@SuperBuilder
+@NoArgsConstructor
+@Getter
+public abstract class BaseEntity {
+
+  @CreatedDate
+  @Column(name = "createdAt", updatable = false, nullable = false)
+  private LocalDateTime createdAt;
+
+  @LastModifiedDate
+  @Column(name = "updatedAt", nullable = false)
+  private LocalDateTime updatedAt;
+
+  @Column(name = "deletedAt")
+  private LocalDateTime deletedAt;
+
+  @Column(name = "isDeleted")
+  private Boolean isDeleted = false;
+
+
+  @PreRemove
+  private void softDelete() {
+    if (isDeleted != null && !isDeleted) {
+      if (deletedAt == null) {
+        deletedAt = LocalDateTime.now();
+      }
+      isDeleted = true;
+    }
+  }
+}
