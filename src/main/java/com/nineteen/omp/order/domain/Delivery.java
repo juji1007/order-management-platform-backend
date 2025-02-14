eb418b14d6d1decbe005434b651c4a06dcba7f75
Choi Hojin <jkhh9725@gmail.com>
2025-02-14 17:02:58 +0900

    feat: Delivery 엔티티 생성 (PK, FK 관계 설정)

---
+++ src/main/java/com/nineteen/omp/order/domain/Delivery.java
@@ -0,0 +1,31 @@
+package com.nineteen.omp.order.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import java.util.UUID;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "p_delivery")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Delivery {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.UUID)
+  private UUID id;
+
+  @OneToOne
+  @JoinColumn(name = "order_id")
+  private Order order;
+}
