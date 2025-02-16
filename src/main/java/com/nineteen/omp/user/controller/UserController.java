22cab4f67e178783e9fe5b96647b83781975dc4b
oh-min <ohyemin96@gmail.com>
2025-02-17 00:15:55 +0900

    Chore(UserController): 불필요한 코드 삭제

--- src/main/java/com/nineteen/omp/user/controller/UserController.java
+++ src/main/java/com/nineteen/omp/user/controller/UserController.java
@@ -8,7 +8,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,11 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
 
   private final UserService userService;
-
-  @GetMapping("/users/signup")
-  public String signPage() {
-    return "get signup success";
-  }
 
   @PostMapping("/users/signup")
   public String signup(
