e1dd15422e2428748582e510087a676eb1c609ab
oh-min <ohyemin96@gmail.com>
2025-02-18 00:02:48 +0900

    Refactor(UserController): @Valid 예외처리방법 변경

--- src/main/java/com/nineteen/omp/user/controller/UserController.java
+++ src/main/java/com/nineteen/omp/user/controller/UserController.java
@@ -4,12 +4,9 @@ import com.nineteen.omp.global.dto.ResponseDto;
 import com.nineteen.omp.user.controller.dto.SignupRequestDto;
 import com.nineteen.omp.user.service.UserService;
 import jakarta.validation.Valid;
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindingResult;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,20 +22,8 @@ public class UserController {
 
   @PostMapping("/users/signup")
   public ResponseEntity<ResponseDto<?>> signup(
-      @RequestBody @Valid SignupRequestDto requestDto,
-      BindingResult bindingResult
+      @RequestBody @Valid SignupRequestDto requestDto
   ) {
-
-    // Validation 예외처리
-    List<FieldError> fieldErrors = bindingResult.getFieldErrors();
-    if (fieldErrors.size() > 0) {
-      for (FieldError fieldError : bindingResult.getFieldErrors()) {
-        log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
-      }
-//      return"signup failed";
-      return null;
-//      return ResponseEntity.badRequest().body(ResponseDto.failure("Signup failed due to validation errors");
-    }
 
     userService.signup(requestDto);
 
