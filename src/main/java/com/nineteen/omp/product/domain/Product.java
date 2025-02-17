78c2ee219f5e39f8971ac647eb7b31763030afaa
Choi Hojin <jkhh9725@gmail.com>
2025-02-17 16:26:34 +0900

    feat: Product 엔티티 컬럼 추가

--- src/main/java/com/nineteen/omp/product/domain/Product.java
+++ src/main/java/com/nineteen/omp/product/domain/Product.java
@@ -1,10 +1,16 @@
 package com.nineteen.omp.product.domain;
 
 
+import com.nineteen.omp.global.entity.BaseEntity;
+import com.nineteen.omp.store.domain.Store;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
@@ -18,9 +24,25 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Product {
+public class Product extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "store_id", nullable = false)
+  private Store store;
+
+  @Column(nullable = false, length = 100)
+  private String name;
+
+  @Column(nullable = false)
+  private Integer price;
+
+  private String image;
+
+  @Column(length = 500)
+  private String description;
+
 }
