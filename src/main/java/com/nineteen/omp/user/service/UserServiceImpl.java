7ac7562abe83696ca8ce8090ef7e9e79d5c50d80
oh-min <ohyemin96@gmail.com>
2025-02-17 15:39:32 +0900

    Refactor: UserService 인터페이스 및 구현 클래스 분리
    - UserService 인터페이스 추가
    - UserServiceImpl 클래스 생성하여 실제 구현 로직 추가
    - 기존 UserService의 비즈니스 로직을 UserServiceImpl로 이동

---
+++ src/main/java/com/nineteen/omp/user/service/UserServiceImpl.java
@@ -0,0 +1,41 @@
+package com.nineteen.omp.user.service;
+
+import com.nineteen.omp.user.controller.dto.SignupRequestDto;
+import com.nineteen.omp.user.domain.User;
+import com.nineteen.omp.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserServiceImpl implements UserService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  @Transactional
+  public void signup(SignupRequestDto requestDto) {
+
+    // 회원 중복 확인
+    boolean checkUsername = userRepository.existsByUsername(requestDto.username());
+    if (checkUsername) {
+      throw new IllegalArgumentException("중복된 사용자가 존재합니다.");
+    }
+    // 사용자 등록
+    User user = User.builder()
+        .username(requestDto.username())
+        .password(requestDto.password())
+        .nickname(requestDto.nickname())
+        .role(requestDto.role())  // 교체 필요
+        .email(requestDto.email())
+        .is_public(requestDto.is_public())
+        .delivery_address(requestDto.delivery_address())
+        .build();
+
+    userRepository.save(user);
+
+  }
+
+}
