bd8e52a557968a4c9c591230d39332a6a12b51fc
oh-min <ohyemin96@gmail.com>
2025-02-18 15:45:57 +0900

    Feat: Security 설정

---
+++ src/main/java/com/nineteen/omp/auth/config/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.nineteen.omp.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http
+        .csrf(csrf -> csrf.disable())
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(auth -> auth
+            .requestMatchers("/", "/api/v1/users/login", "/api/v1/users/signup")
+            .permitAll()
+            .anyRequest().authenticated()
+        );
+
+    // JWT 필터 추가
+
+    return http.build();
+  }
+
+}
