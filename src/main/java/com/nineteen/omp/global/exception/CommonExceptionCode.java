ed44312cbef7c0143b24c281bed43074e3fa3344
oh-min <ohyemin96@gmail.com>
2025-02-18 12:46:42 +0900

    Feat: 사용자 조회 시 USER_NOT_FOUND 예외 추가

--- src/main/java/com/nineteen/omp/global/exception/CommonExceptionCode.java
+++ src/main/java/com/nineteen/omp/global/exception/CommonExceptionCode.java
@@ -12,6 +12,7 @@ public enum CommonExceptionCode implements ExceptionCode {
   INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 문제가 발생되었습니다."),
   PAGE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 페이지를 찾을 수 없습니다."),
   DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "중복된 사용자가 존재합니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
   ;
 
   private final HttpStatus httpStatus;
