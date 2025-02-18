1391fa4b8ba03e844b19f1954b3a7aafd5897e11
oh-min <ohyemin96@gmail.com>
2025-02-18 12:54:56 +0900

    Feat: 사용자 조회 메서드 추가

--- src/main/java/com/nineteen/omp/user/repository/UserRepository.java
+++ src/main/java/com/nineteen/omp/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.nineteen.omp.user.domain.User;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
