dad18990e71cfd3539c946b507542a4494cb3eed
jujail <juji1007@hanmail.net>
2025-02-18 10:13:08 +0900

    Feat: store 엔티티 추가

--- src/main/java/com/nineteen/omp/store/domain/Store.java
+++ src/main/java/com/nineteen/omp/store/domain/Store.java
@@ -1,8 +1,8 @@
 package com.nineteen.omp.store.domain;
 
-
 import com.nineteen.omp.category.domain.StoreCategory;
 import com.nineteen.omp.user.domain.User;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,6 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.time.LocalTime;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -26,13 +27,29 @@ public class Store {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "store_id", updatable = false, nullable = false)
   private UUID id;
 
   @OneToOne
-  @JoinColumn(name = "user_id")
+  @JoinColumn(name = "user_id", nullable = false)
   private User user;
 
   @OneToOne
-  @JoinColumn(name = "category_id")
+  @JoinColumn(name = "category_id", nullable = false)
   private StoreCategory storeCategory;
+
+  @Column(name = "name", updatable = true, nullable = false)
+  private String name;
+  @Column(name = "address", updatable = true, nullable = false)
+  private String address;
+  @Column(name = "phone", updatable = true, nullable = false)
+  private String phone;
+
+  @Column(name = "open_hours", updatable = true, nullable = true)
+  private LocalTime openHours;
+  @Column(name = "close_hours", updatable = true, nullable = true)
+  private LocalTime closeHours;
+  @Column(name = "closed_days", updatable = true, nullable = true)
+  private String closedDays; // -> localDate 고려 리스트도 고려
+
 }
