3242d9c6cb03b5b5546f519e9eb1d039000824c3
jeonjuho <wjswngh23@daum.net>
2025-02-19 18:08:38 +0900

    feat(UserCouponService): UserCoupon 도메인의 서비스 인터페이스 추가

---
+++ src/main/java/com/nineteen/omp/coupon/service/UserCouponService.java
@@ -0,0 +1,8 @@
+package com.nineteen.omp.coupon.service;
+
+import com.nineteen.omp.coupon.domain.UserCoupon;
+
+public interface UserCouponService {
+
+  UserCoupon getUserCoupon(String userCouponId);
+}
