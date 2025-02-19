9ae004c34a41bc24fa8c63b5b4b885c1228414d9
jujail <juji1007@hanmail.net>
2025-02-19 17:38:26 +0900

    refactor: area entity 기능 추가 - softDelete - update 구현 생성자

--- src/main/java/com/nineteen/omp/store/domain/Area.java
+++ src/main/java/com/nineteen/omp/store/domain/Area.java
@@ -1,6 +1,8 @@
 package com.nineteen.omp.store.domain;
 
 import com.nineteen.omp.global.entity.BaseEntity;
+import com.nineteen.omp.store.exception.AreaException;
+import com.nineteen.omp.store.exception.AreaExceptionCode;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,6 +14,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
 
 @Entity
 @Table(name = "p_area")
@@ -19,10 +23,13 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@SQLRestriction("is_deleted = false")
+@SQLDelete(sql = "UPDATE p_area SET is_deleted = true WHERE area_id = ?")
 public class Area extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
+  @Column(name = "area_id", nullable = false)
   private UUID id;
 
   @Column(name = "si", nullable = false)
@@ -33,7 +40,25 @@ public class Area extends BaseEntity {
 
   @Column(name = "dong", nullable = false)
   private String dong;
+
+  public void changeNameSi(String si) {
+    if (si == null) {
+      throw new AreaException(AreaExceptionCode.AREA_SI_IS_NULL);
+    }
+    this.si = si;
+  }
+
+  public void changeNameGu(String gu) {
+    if (gu == null) {
+      throw new AreaException(AreaExceptionCode.AREA_GU_IS_NULL);
+    }
+    this.gu = gu;
+  }
+
+  public void changeNameDong(String dong) {
+    if (dong == null) {
+      throw new AreaException(AreaExceptionCode.AREA_DONG_IS_NULL);
+    }
+    this.dong = dong;
+  }
 }
-
-
-
