afa0db37e5ad8dcdce16872928e0acaa64768d2a
oh-min <ohyemin96@gmail.com>
2025-02-20 00:39:52 +0900

    Feat: 로그인 기능 구현

---
+++ src/main/java/com/nineteen/omp/auth/filter/JwtAuthenticationFilter.java
@@ -0,0 +1,66 @@
+package com.nineteen.omp.auth.filter;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nineteen.omp.global.exception.CustomException;
+import com.nineteen.omp.user.controller.dto.LoginRequestDto;
+import com.nineteen.omp.user.domain.UserExceptionCode;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+  private final AuthenticationManager authenticationManager;
+
+  public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+    this.authenticationManager = authenticationManager;
+    setFilterProcessesUrl("/api/v1/users/login");
+  }
+
+  @Override
+  public Authentication attemptAuthentication(HttpServletRequest request,
+      HttpServletResponse response) throws AuthenticationException {
+    try {
+      String requestBody = new String(request.getInputStream().readAllBytes());
+
+      LoginRequestDto loginRequest = new ObjectMapper().readValue(requestBody,
+          LoginRequestDto.class);
+
+      UsernamePasswordAuthenticationToken authenticationToken =
+          new UsernamePasswordAuthenticationToken(loginRequest.username(),
+              loginRequest.password());
+
+      return authenticationManager.authenticate(authenticationToken);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    } catch (AuthenticationException e) {
+      throw new CustomException(UserExceptionCode.USER_NOT_FOUND);
+    }
+  }
+
+  // 로그인 성공 시 호출
+  @Override
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+      FilterChain chain, Authentication authResult) throws IOException, ServletException {
+    response.setContentType("application/json");
+    response.getWriter().write("{\"message\": \"login success!\"}");
+  }
+
+  // 로그인 실패 시 호출
+  @Override
+  protected void unsuccessfulAuthentication(HttpServletRequest request,
+      HttpServletResponse response, AuthenticationException failed)
+      throws IOException, ServletException {
+    response.setContentType("application/json");
+    response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    response.getWriter().write("{\"message\": \"login failed!\"}");
+  }
+
+}
--- src/main/java/com/nineteen/omp/user/domain/UserExceptionCode.java
+++ src/main/java/com/nineteen/omp/user/domain/UserExceptionCode.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum UserExceptionCode implements ExceptionCode {
 
   DUPLICATE_USERNAME(HttpStatus.BAD_REQUEST, "중복된 사용자가 존재합니다."),
+  USER_NOT_FOUND(HttpStatus.NOT_FOUND, "사용자를 찾을 수 없습니다."),
+  INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다."),
   ;
 
   private final HttpStatus httpStatus;
