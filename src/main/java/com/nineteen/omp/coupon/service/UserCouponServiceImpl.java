914bb4f59b8df92b384a331406d5f96f6627a83a
jeonjuho <wjswngh23@daum.net>
2025-02-19 18:09:55 +0900

    feat(UserCouponServiceImpl): 사용자 쿠폰 조회 서비스 로직 추가

---
+++ src/main/java/com/nineteen/omp/coupon/service/UserCouponServiceImpl.java
@@ -0,0 +1,24 @@
+package com.nineteen.omp.coupon.service;
+
+import com.nineteen.omp.coupon.domain.UserCoupon;
+import com.nineteen.omp.coupon.exception.UserCouponExceptionCode;
+import com.nineteen.omp.coupon.repository.UserCouponRepository;
+import com.nineteen.omp.global.exception.CustomException;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserCouponServiceImpl implements UserCouponService {
+
+  private final UserCouponRepository userCouponRepository;
+
+  @Override
+  public UserCoupon getUserCoupon(String userCouponId) {
+    return userCouponRepository.findById(UUID.fromString(userCouponId))
+        .orElseThrow(() -> new CustomException(UserCouponExceptionCode.USER_COUPON_NOT_FOUND));
+  }
+}
