6e12171c4b86949963b16bc07368953e129fe6b1
jujail <juji1007@hanmail.net>
2025-02-19 12:58:10 +0900

    feat: area entity 추가

--- src/main/java/com/nineteen/omp/store/domain/Area.java
+++ src/main/java/com/nineteen/omp/store/domain/Area.java
@@ -1,6 +1,7 @@
 package com.nineteen.omp.store.domain;
 
-
+import com.nineteen.omp.global.entity.BaseEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -18,9 +19,21 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Area {
+public class Area extends BaseEntity {
 
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
   private UUID id;
+
+  @Column(name = "si", nullable = false)
+  private String si;
+
+  @Column(name = "gu", nullable = false)
+  private String gu;
+
+  @Column(name = "dong", nullable = false)
+  private String dong;
 }
+
+
+
