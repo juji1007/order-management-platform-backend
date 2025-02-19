9ea1a8b9c04d60088424a6d25c92f029d7873383
jeonjuho <wjswngh23@daum.net>
2025-02-19 09:39:10 +0900

    feat(UserRepository): User 엔티티 레포지토리 추가

---
+++ src/main/java/com/nineteen/omp/user/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.nineteen.omp.user.repository;
+
+
+import com.nineteen.omp.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
