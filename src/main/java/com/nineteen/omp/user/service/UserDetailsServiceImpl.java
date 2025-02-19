ce3125052919375964f8ffc627a6a98c7c1d50b7
oh-min <ohyemin96@gmail.com>
2025-02-20 00:39:52 +0900

    Feat: 로그인 기능 구현

---
+++ src/main/java/com/nineteen/omp/user/service/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.nineteen.omp.user.service;
+
+import com.nineteen.omp.auth.dto.UserDetailsImpl;
+import com.nineteen.omp.user.domain.User;
+import com.nineteen.omp.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+  private final UserRepository userRepository;
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    User user = userRepository.findByUsername(username)
+        .orElseThrow(() -> new UsernameNotFoundException("사용자를 찾을 수 없습니다: " + username));
+
+    return new UserDetailsImpl(user);
+  }
+}
