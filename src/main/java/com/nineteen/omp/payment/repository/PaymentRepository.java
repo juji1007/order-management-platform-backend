3d9b309b9f68ebebb205834199e5ac1556e53401
jeonjuho <wjswngh23@daum.net>
2025-02-19 21:23:25 +0900

    feat(PaymentRepository): 주문으로 결제 조회 레포지토리 메서드 추가

--- src/main/java/com/nineteen/omp/payment/repository/PaymentRepository.java
+++ src/main/java/com/nineteen/omp/payment/repository/PaymentRepository.java
@@ -1,9 +1,11 @@
 package com.nineteen.omp.payment.repository;
 
 import com.nineteen.omp.payment.domain.Payment;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
 
+  Optional<Payment> findByOrderId(UUID orderId);
 }
