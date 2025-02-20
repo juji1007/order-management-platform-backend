a3be9d9ef82649e3c14c5d566f9c60172bf86711
jeonjuho <wjswngh23@daum.net>
2025-02-20 15:39:03 +0900

    feat(UserController): 사용자 정보 조회 컨트롤러 추가

--- src/main/java/com/nineteen/omp/user/controller/UserController.java
+++ src/main/java/com/nineteen/omp/user/controller/UserController.java
@@ -1,10 +1,15 @@
 package com.nineteen.omp.user.controller;
 
+import com.nineteen.omp.global.dto.ResponseDto;
+import com.nineteen.omp.user.controller.dto.GetUserInfoResponseDto;
 import com.nineteen.omp.user.controller.dto.SignupRequestDto;
 import com.nineteen.omp.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,4 +32,12 @@ public class UserController {
 
   }
 
+  @GetMapping("/users/{userId}")
+  public ResponseEntity<ResponseDto<?>> getUserInfo(
+      @PathVariable(name = "userId") Long userId
+  ) {
+    var responseCommand = userService.getUserInfo(userId);
+    var response = new GetUserInfoResponseDto(responseCommand);
+    return ResponseEntity.ok(ResponseDto.success(response));
+  }
 }
