9c9648d195e02caa0a1f6a3e9f78ac3cd42e675a
jeonjuho <wjswngh23@daum.net>
2025-02-20 16:15:28 +0900

    feat(UserController): 모든 사용자 정보 조회 컨트롤러 추가

--- src/main/java/com/nineteen/omp/user/controller/UserController.java
+++ src/main/java/com/nineteen/omp/user/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.nineteen.omp.user.controller;
 
 import com.nineteen.omp.global.dto.ResponseDto;
+import com.nineteen.omp.user.controller.dto.GetUserInfoPageResponseDto;
 import com.nineteen.omp.user.controller.dto.GetUserInfoResponseDto;
 import com.nineteen.omp.user.controller.dto.SignupRequestDto;
 import com.nineteen.omp.user.service.UserService;
+import com.nineteen.omp.user.service.dto.GetUserInfoPageResponseCommand;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,4 +46,36 @@ public class UserController {
     var response = new GetUserInfoResponseDto(responseCommand);
     return ResponseEntity.ok(ResponseDto.success(response));
   }
+
+  @GetMapping("/users/all")
+  public ResponseEntity<ResponseDto<?>> getUsers(
+      @PageableDefault(
+          size = 10,
+          page = 1,
+          sort = {"createdAt", "updatedAt"},
+          direction = Direction.ASC
+      ) Pageable pageable
+  ) {
+    // Master 만 조회 가능
+//    PageableUtils.validatePageable(pageable);
+    var responseCommand = userService.getUsers(pageable);
+    var responseDto = convertCommandToDto(pageable, responseCommand);
+    return ResponseEntity.ok(ResponseDto.success(responseDto));
+  }
+
+  private static GetUserInfoPageResponseDto convertCommandToDto(
+      Pageable pageable,
+      GetUserInfoPageResponseCommand responseCommand
+  ) {
+    var responseDtoList = responseCommand.getUserPageResponseCommandPage().stream()
+        .map(GetUserInfoResponseDto::new)
+        .toList();
+    var responseDtoPage = new PageImpl<>(
+        responseDtoList,
+        pageable,
+        responseCommand.getUserPageResponseCommandPage().getTotalElements()
+    );
+    return new GetUserInfoPageResponseDto(responseDtoPage);
+  }
+
 }
