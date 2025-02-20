cb7a42a63f289277a9ee41e2dc8987371c3ad0e3
Choi Hojin <jkhh9725@gmail.com>
2025-02-21 07:42:42 +0900

    feat: 주문 예외 처리 로직 추가 (OrderException 추가)

---
+++ src/main/java/com/nineteen/omp/order/exception/OrderExceptionCode.java
@@ -0,0 +1,17 @@
+package com.nineteen.omp.order.exception;
+
+import com.nineteen.omp.global.exception.ExceptionCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum OrderExceptionCode implements ExceptionCode {
+  ORDER_NOT_FOUND(HttpStatus.NOT_FOUND, "주문을 찾을 수 없습니다."),
+  ORDER_CREATION_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "주문 생성에 실패했습니다.");
+
+
+  private final HttpStatus httpStatus;
+  private final String message;
+}
