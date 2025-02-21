ec79821e66778cd6091572a5bab66be72eff71fe
jujail <juji1007@hanmail.net>
2025-02-22 04:07:07 +0900

    refactor(storeExceptionCode) : 불필요한 내용 제거

--- src/main/java/com/nineteen/omp/store/exception/StoreExceptionCode.java
+++ src/main/java/com/nineteen/omp/store/exception/StoreExceptionCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum StoreExceptionCode implements ExceptionCode {
-  //가게 중복 등록?, 운영시간 등록?(시간 값 오류)
+
   STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 가게를 찾을 수 없습니다."),
   STORE_IS_DUPLICATED(HttpStatus.BAD_REQUEST, "이미 존재하는 가게 입니다."),
   STORE_CATEGORY_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않은 카테고리 입니다."),
@@ -17,17 +17,8 @@ public enum StoreExceptionCode implements ExceptionCode {
   STORE_ADDRESS_IS_NULL(HttpStatus.BAD_REQUEST, "스토어 주소가 null입니다."),
   STORE_PHONE_IS_NULL(HttpStatus.BAD_REQUEST, "스토어 전화번호가 null입니다."),
   STORE_DELETE_FAIL(HttpStatus.BAD_REQUEST, "삭제에 실패하였습니다.");
+
   private final HttpStatus httpStatus;
   private final String message;
-
-  @Override
-  public HttpStatus getHttpStatus() {
-    return this.httpStatus;
-  }
-
-  @Override
-  public String getMessage() {
-    return this.message;
-  }
 
 }
