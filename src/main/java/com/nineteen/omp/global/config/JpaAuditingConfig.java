f9f150ec38259a3fdbb948b2a163e99d9c86ee3a
jeonjuho <wjswngh23@daum.net>
2025-02-21 21:24:22 +0900

    feat(JpaAuditingConfig): JpaAuditing 설정 추가
    - 인증된 사용자를 SecurityContextHolder에서 가져올 수 있도록 auditorProvider() 추가

--- src/main/java/com/nineteen/omp/global/config/JpaAuditingConfig.java
+++ src/main/java/com/nineteen/omp/global/config/JpaAuditingConfig.java
@@ -1,10 +1,27 @@
 package com.nineteen.omp.global.config;
 
+import com.nineteen.omp.auth.dto.UserDetailsImpl;
+import java.util.Optional;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 @Configuration
-@EnableJpaAuditing
+@EnableJpaAuditing(auditorAwareRef = "auditorProvider")
 public class JpaAuditingConfig {
 
+  @Bean
+  public AuditorAware<Long> auditorProvider() {
+    return () -> {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+      if (authentication == null || !authentication.isAuthenticated()) {
+        return Optional.empty();
+      }
+      UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+      return Optional.of(principal.getUserId());
+    };
+  }
 }
