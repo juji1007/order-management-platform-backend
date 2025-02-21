31183a44ad9c4d9ba2a0cb66f5cdc9403e06703b
Choi Hojin <jkhh9725@gmail.com>
2025-02-21 09:32:36 +0900

    feat: Order 엔티티 컬럼 및 설정 추가 - List<OrderProduct> orderProducts 추가 - cancelOrder 메서드 추가 - toBuilder 설정 추가

--- src/main/java/com/nineteen/omp/order/domain/Order.java
+++ src/main/java/com/nineteen/omp/order/domain/Order.java
@@ -15,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import java.util.List;
 import java.util.UUID;
@@ -28,7 +29,7 @@ import org.hibernate.annotations.SQLRestriction;
 @Entity
 @Table(name = "p_order")
 @Getter
-@Builder
+@Builder(toBuilder = true)
 @NoArgsConstructor
 @AllArgsConstructor
 @SQLRestriction("is_deleted = false")
@@ -55,6 +56,8 @@ public class Order extends BaseEntity {
   @Enumerated(EnumType.STRING)
   private OrderType orderType;
 
+  @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+  private List<OrderProduct> orderProducts;
 
   @Builder
   public Order(Store store, User user, int totalPrice, OrderStatus orderStatus,
@@ -72,5 +75,11 @@ public class Order extends BaseEntity {
         .mapToInt(
             orderProduct -> orderProduct.getQuantity() * orderProduct.getStoreProduct().getPrice())
         .sum();
+  }
+
+  public Order cancelOrder() {
+    return this.toBuilder()
+        .orderStatus(OrderStatus.CANCELLED)
+        .build();
   }
 }
