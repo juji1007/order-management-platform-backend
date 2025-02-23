6c9e5494d5f8fd322c18ae34cb80002ea1c40328
jujail <juji1007@hanmail.net>
2025-02-24 04:25:12 +0900

    refactor : 유저 시큐리티 적용, 유저 exceptionCode 적용

--- src/main/java/com/nineteen/omp/coupon/repository/dto/UserCouponData.java
+++ src/main/java/com/nineteen/omp/coupon/repository/dto/UserCouponData.java
@@ -2,15 +2,17 @@ package com.nineteen.omp.coupon.repository.dto;
 
 import com.nineteen.omp.coupon.domain.Coupon;
 import com.nineteen.omp.coupon.domain.UserCoupon;
+import com.nineteen.omp.user.domain.User;
 
 public record UserCouponData(
-    Long userId,
+    User user,
     Coupon coupon,
     boolean status
 ) {
 
   public UserCoupon toEntity() {
     return UserCoupon.builder()
+        .user(user)
         .coupon(coupon)
         .status(status)
         .build();
