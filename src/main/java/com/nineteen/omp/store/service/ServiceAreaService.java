e0e3db831ab319c67f249739d763443c247f5db7
jujail <juji1007@hanmail.net>
2025-02-23 18:02:11 +0900

    refactor : 리뷰 반영

--- src/main/java/com/nineteen/omp/store/service/ServiceAreaService.java
+++ src/main/java/com/nineteen/omp/store/service/ServiceAreaService.java
@@ -1,20 +1,11 @@
 package com.nineteen.omp.store.service;
 
+import com.nineteen.omp.store.controller.dto.ServiceAreaRequestDto;
 import com.nineteen.omp.store.controller.dto.ServiceAreaResponseDto;
-import com.nineteen.omp.store.domain.ServiceArea;
-import com.nineteen.omp.store.exception.ServiceAreaException;
-import com.nineteen.omp.store.exception.ServiceAreaExceptionCode;
-import com.nineteen.omp.store.repository.ServiceAreaRepository;
-import com.nineteen.omp.store.repository.dto.ServiceAreaData;
-import com.nineteen.omp.store.service.dto.ServiceAreaCommand;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 public interface ServiceAreaService {
 
@@ -24,5 +15,5 @@ public interface ServiceAreaService {
 
   List<ServiceAreaResponseDto> getAreasByStoreId(UUID storeId);
 
-  ServiceAreaResponseDto createServiceArea(ServiceAreaCommand serviceAreaCommand);
+  ServiceAreaResponseDto createServiceArea(ServiceAreaRequestDto serviceAreaRequestDto);
 }
--- src/main/java/com/nineteen/omp/store/service/ServiceAreaServiceImpl.java
+++ src/main/java/com/nineteen/omp/store/service/ServiceAreaServiceImpl.java
@@ -1,12 +1,22 @@
 package com.nineteen.omp.store.service;
 
+import static com.nineteen.omp.store.controller.dto.ServiceAreaResponseDto.toResponseDto;
+
+import com.nineteen.omp.store.controller.dto.ServiceAreaRequestDto;
 import com.nineteen.omp.store.controller.dto.ServiceAreaResponseDto;
+import com.nineteen.omp.store.domain.Area;
 import com.nineteen.omp.store.domain.ServiceArea;
+import com.nineteen.omp.store.domain.Store;
+import com.nineteen.omp.store.exception.AreaException;
+import com.nineteen.omp.store.exception.AreaExceptionCode;
 import com.nineteen.omp.store.exception.ServiceAreaException;
 import com.nineteen.omp.store.exception.ServiceAreaExceptionCode;
+import com.nineteen.omp.store.exception.StoreException;
+import com.nineteen.omp.store.exception.StoreExceptionCode;
+import com.nineteen.omp.store.repository.AreaRepository;
 import com.nineteen.omp.store.repository.ServiceAreaRepository;
+import com.nineteen.omp.store.repository.StoreRepository;
 import com.nineteen.omp.store.repository.dto.ServiceAreaData;
-import com.nineteen.omp.store.service.dto.ServiceAreaCommand;
 import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
@@ -18,28 +28,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ServiceAreaServiceImpl implements ServiceAreaService {
 
   private final ServiceAreaRepository serviceAreaRepository;
+  private final AreaRepository areaRepository;
+  private final StoreRepository storeRepository;
 
   @Transactional
-  public ServiceAreaResponseDto createServiceArea(ServiceAreaCommand serviceAreaCommand) {
+  public ServiceAreaResponseDto createServiceArea(ServiceAreaRequestDto serviceAreaRequestDto) {
+
+    Area area = areaRepository.findById(serviceAreaRequestDto.areaId())
+        .orElseThrow(() -> new AreaException(AreaExceptionCode.AREA_NOT_FOUND));
+
+    Store store = storeRepository.findById(serviceAreaRequestDto.storeId())
+        .orElseThrow(() -> new StoreException(StoreExceptionCode.STORE_NOT_FOUND));
+
     boolean isExists = serviceAreaRepository.existsByAreaIdAndStoreId(
-        serviceAreaCommand.area().getId(),
-        serviceAreaCommand.store().getId());
+        area.getId(),
+        store.getId());
     if (isExists) {
       throw new ServiceAreaException(ServiceAreaExceptionCode.SERVICE_AREA_IS_DUPLICATED);
     }
 
-    ServiceAreaData serviceAreaData = new ServiceAreaData(serviceAreaCommand.area(),
-        serviceAreaCommand.store());
-
+    ServiceAreaData serviceAreaData = new ServiceAreaData(area, store);
     ServiceArea savedServiceArea = serviceAreaRepository.save(serviceAreaData.toEntity());
 
     return toResponseDto(savedServiceArea);
   }
 
-  @Transactional(readOnly = true)
   public List<ServiceAreaResponseDto> getAreasByStoreId(UUID storeId) {
     boolean isExists = serviceAreaRepository.existsByStoreId(storeId);
     if (!isExists) {
@@ -53,11 +70,11 @@ public class ServiceAreaServiceImpl implements ServiceAreaService {
     }
 
     return serviceAreas.stream()
-        .map(this::toResponseDto)
+//        .map(serviceArea -> ServiceAreaResponseDto.toResponseDto(serviceArea))
+        .map(ServiceAreaResponseDto::toResponseDto)
         .collect(Collectors.toList());
   }
 
-  @Transactional(readOnly = true)
   public Page<ServiceAreaResponseDto> getStoresByAreaId(UUID areaId, Pageable pageable) {
     return serviceAreaRepository.getStoresByAreaId(areaId, pageable);
   }
@@ -69,14 +86,6 @@ public class ServiceAreaServiceImpl implements ServiceAreaService {
             () -> new ServiceAreaException(ServiceAreaExceptionCode.SERVICE_AREA_NOT_FOUND));
 
     serviceAreaRepository.delete(serviceArea);
-  }
-
-  private ServiceAreaResponseDto toResponseDto(ServiceArea serviceArea) {
-    return new ServiceAreaResponseDto(
-        serviceArea.getId(),
-        serviceArea.getArea().getId(),
-        serviceArea.getStore().getId()
-    );
   }
 
 }
