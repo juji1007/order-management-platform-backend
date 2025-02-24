662952bb13f8a4c0dde1589c56ab7d62c86ac73d
oh-min <ohyemin96@gmail.com>
2025-02-24 16:05:02 +0900

    feat: user 도메인 권한 설정
    - 전체사용자 정보 조회 권한 설정 (관리자)
    - 회원검색 권한설정 (관리자)

--- src/main/java/com/nineteen/omp/user/controller/UserController.java
+++ src/main/java/com/nineteen/omp/user/controller/UserController.java
@@ -16,6 +16,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -50,6 +51,7 @@ public class UserController {
     return ResponseEntity.ok(ResponseDto.success(response));
   }
 
+  @PreAuthorize("hasRole('MASTER')")
   @GetMapping("/users/all")
   public ResponseEntity<ResponseDto<?>> getUsers(
       @PageableDefault(
@@ -83,7 +85,8 @@ public class UserController {
     userService.deleteUser(userId);
     return ResponseEntity.ok(ResponseDto.success());
   }
-
+  
+  @PreAuthorize("hasRole('MASTER')")
   @GetMapping("/users")
   public ResponseEntity<ResponseDto<?>> searchUser(
       @RequestParam(
