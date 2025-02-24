deeedba6a018c68c4864ff4cc6bf86d8b5cddea3
jeonjuho <wjswngh23@daum.net>
2025-02-25 04:24:06 +0900

    feat(PaymentExceptionCode): 결제 도메인 예외 처리 추가

--- src/main/java/com/nineteen/omp/payment/exception/PaymentExceptionCode.java
+++ src/main/java/com/nineteen/omp/payment/exception/PaymentExceptionCode.java
@@ -10,6 +10,10 @@ import org.springframework.http.HttpStatus;
 public enum PaymentExceptionCode implements ExceptionCode {
 
   NOT_FOUND_PAYMENT(HttpStatus.BAD_REQUEST, "결제 정보를 찾을 수 없습니다."),
+  NOT_OWNER_PAYMENT(HttpStatus.BAD_REQUEST, "해당 주문의 가게 주인이 아닙니다."),
+  NOT_USER_PAYMENT(HttpStatus.BAD_REQUEST, "해당 주문의 유저가 아닙니다."),
+  NOT_VALID_CANCEL_REQUEST(HttpStatus.BAD_REQUEST, "취소 요청이 유효하지 않습니다."),
+  OVER_TIME_CANCEL_REQUEST(HttpStatus.BAD_REQUEST, "취소 요청이 시간을 초과하였습니다."),
   ;
 
   private final HttpStatus httpStatus;
