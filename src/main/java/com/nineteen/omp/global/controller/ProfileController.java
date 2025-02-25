17190d6d2a5c4ea658a0189c435dd923a8ef1fe1
jeonjuho <wjswngh23@daum.net>
2025-02-25 21:05:52 +0900

    feat: profile 조회 가능하도록 보안 설정 수정

--- src/main/java/com/nineteen/omp/global/controller/ProfileController.java
+++ src/main/java/com/nineteen/omp/global/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package com.nineteen.omp.global.controller;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,11 +7,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1")
-@RequiredArgsConstructor
 public class ProfileController {
 
   @Value("${spring.profiles.active}")
-  private final String profile;
+  private String profile;
 
   @GetMapping("/profile")
   public String getProfile() {
