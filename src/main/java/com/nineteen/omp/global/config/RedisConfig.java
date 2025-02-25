0b7efe0e192bad69132d555fbfceb70a50d20f89
jeonjuho <wjswngh23@daum.net>
2025-02-26 06:27:08 +0900

    feat: redis 설정 수정

--- src/main/java/com/nineteen/omp/global/config/RedisConfig.java
+++ src/main/java/com/nineteen/omp/global/config/RedisConfig.java
@@ -27,6 +27,7 @@ public class RedisConfig {
   @Bean
   public RedisTemplate<?, ?> redisTemplate() {
     RedisTemplate<byte[], byte[]> redisTemplate = new RedisTemplate<>();
+    redisTemplate.setConnectionFactory(redisConnectionFactory());
     redisTemplate.setKeySerializer(new StringRedisSerializer());
     redisTemplate.setValueSerializer(new StringRedisSerializer());
     redisTemplate.setConnectionFactory(redisConnectionFactory());
